4333c26f003752ef7c032e4af779a75ef04e44cb
Alex Ch <aleks00799@gmail.com>
2023-07-29 13:42:15 +0300

    update to avalonia 11

--- DesktopNotifications.Avalonia/AppBuilderExtensions.cs
+++ DesktopNotifications.Avalonia/AppBuilderExtensions.cs
@@ -4,11 +4,12 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Platform;
 using DesktopNotifications.FreeDesktop;
 using DesktopNotifications.Windows;
+using System;
 
 namespace DesktopNotifications.Avalonia
 {
     /// <summary>
-    /// Extensions for <see cref="AppBuilderBase{TAppBuilder}" />
+    /// Extensions for <see cref="AppBuilder" />
     /// </summary>
     public static class AppBuilderExtensions
     {
@@ -16,47 +17,38 @@ namespace DesktopNotifications.Avalonia
         /// Setups the <see cref="INotificationManager" /> for the current platform and
         /// binds it to the service locator (<see cref="AvaloniaLocator" />).
         /// </summary>
-        /// <typeparam name="TAppBuilder"></typeparam>
         /// <param name="builder"></param>
         /// <returns></returns>
-        public static TAppBuilder SetupDesktopNotifications<TAppBuilder>(this TAppBuilder builder)
-            where TAppBuilder : AppBuilderBase<TAppBuilder>, new()
+        public static AppBuilder SetupDesktopNotifications(this AppBuilder builder, out INotificationManager? manager)
         {
-            INotificationManager manager;
-            var runtimeInfo = builder.RuntimePlatform.GetRuntimeInfo();
-
-            switch (runtimeInfo.OperatingSystem)
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
             {
-                case OperatingSystemType.WinNT:
-                {
-                    var context = WindowsApplicationContext.FromCurrentProcess();
-                    manager = new WindowsNotificationManager(context);
-                    break;
-                }
-
-                case OperatingSystemType.Linux:
-                {
-                    var context = FreeDesktopApplicationContext.FromCurrentProcess();
-                    manager = new FreeDesktopNotificationManager(context);
-                    break;
-                }
-
+                var context = WindowsApplicationContext.FromCurrentProcess();
+                manager = new WindowsNotificationManager(context);
+            }
+            else if (Environment.OSVersion.Platform == PlatformID.Unix)
+            {
+                var context = FreeDesktopApplicationContext.FromCurrentProcess();
+                manager = new FreeDesktopNotificationManager(context);
+            }
+            else
+            {
                 //TODO: OSX once implemented/stable
-                default: return builder;
+                manager = null;
+                return builder;
             }
 
             //TODO Any better way of doing this?
             manager.Initialize().GetAwaiter().GetResult();
 
+            var manager_ = manager;
             builder.AfterSetup(b =>
             {
-                if (b.Instance.ApplicationLifetime is IControlledApplicationLifetime lifetime)
+                if (b.Instance?.ApplicationLifetime is IControlledApplicationLifetime lifetime)
                 {
-                    lifetime.Exit += (s, e) => { manager.Dispose(); };
+                    lifetime.Exit += (s, e) => { manager_.Dispose(); };
                 }
             });
-
-            AvaloniaLocator.CurrentMutable.Bind<INotificationManager>().ToConstant(manager);
 
             return builder;
         }
