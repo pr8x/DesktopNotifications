d7a2981838a1d707e554965f304ebc9a95e93a5a
Alex <aleks00799@gmail.com>
2023-10-21 23:42:37 +0300

    Update to av11 (#23)
    * Bump avalonia version to 11.0.1
    
    * update to avalonia 11
    
    * update example to avalonia 11

--- Example.Avalonia/Program.cs
+++ Example.Avalonia/Program.cs
@@ -1,10 +1,13 @@
 using Avalonia;
+using DesktopNotifications;
 using DesktopNotifications.Avalonia;
 
 namespace Example.Avalonia
 {
     internal class Program
     {
+        public static INotificationManager NotificationManager = null!;
+
         public static void Main(string[] args)
         {
             BuildAvaloniaApp()
@@ -15,7 +18,7 @@ namespace Example.Avalonia
         {
             return AppBuilder.Configure<App>()
                 .UsePlatformDetect()
-                .SetupDesktopNotifications()
+                .SetupDesktopNotifications(out NotificationManager!)
                 .LogToTrace();
         }
     }
